03f6625b5922b6e4e963000ab6cbcdbd14ca1ba2
tedlasiri29 <tedlasiri@gmail.com>
2026-02-02 15:49:46 +0530

    Add files via upload

---
+++ summer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int temp;
+int summer;
+printf("enter the temparature \n");
+scanf("%d",&temp);
+printf("enter 1 for summer\t 0 for not summer \n");
+scanf("%d",&summer);
+if(summer)
+{
+if(temp>=60 && temp<=100)
+{
+printf("true");
+}
+else
+{
+printf("false");
+}
+    }
+    }
