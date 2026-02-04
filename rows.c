9aa63f211c44649928a3a336a3cd6521350f0b8c
tedlasiri29 <tedlasiri@gmail.com>
2026-02-04 11:16:08 +0530

    Add files via upload

---
+++ rows.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    printf("enter the number of rows \n");
+    scanf("%d",&rows);
+    for(int i=1;i<=rows;i++)
+    {
+        printf("* \n");
+    }
+}
